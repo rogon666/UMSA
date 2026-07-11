e88d600432c7d52bdc766f593e398ea03e276843
Rolando Gonzales Martinez <62504422+rogon666@users.noreply.github.com>
2026-07-11 11:37:39 +0200

    July 11

---
+++ 2026/MLMLG_2026/codigos/R/UMSA_MLMLG_2026_002.R
@@ -0,0 +1,61 @@
+# ==========================================================
+# Maestría en Ciencia y Análisis de Datos
+# Universidad Mayor de San Andrés
+# ----------------------------------------------------------
+#   Modelos lineales y modelos lineales generalizados
+# ----------------------------------------------------------
+#        Rolando Gonzales Martinez, Julio 2026
+# ==========================================================
+
+# Simulaciones de un modelo lineal univariado 
+
+# Libreria: 
+library(ggplot2)
+
+# Definiendo semilla:
+set.seed(333)
+
+# Genero datos artificiales aleatorios:
+n <- 1000  # numero de observaciones
+mu <- 50
+sigma <- 10
+x <- rnorm(n, mean = mu, sd = sigma)  # Predictor variable N(mu,sigma2)
+hist(x)
+
+# y = beta0 + beta1*x + e
+
+errores = rnorm(n, mean = 0, sd = 15)  # Errores: e ~ N(0,15)
+
+# Parametros poblacionales:
+beta0 =  # 5
+beta1 =  # 2
+
+# y = beta0 + beta1*x + e
+
+# Combinar los datos x,y en un dataframe:
+datos <- data.frame(x = x, y = y)
+
+# Ajuste de un modelo de regresion lineal con una sola variable explicativa:
+modelo_lineal <- (y ~ x, data = datos) #lm
+
+# Resultados del modelo
+summary(modelo_lineal)
+
+# Grafico de datos:
+ggplot(datos, aes(x = x, y = y)) +
+  geom_point(color = "yellow") +
+  geom_smooth(method = "lm", color = "lightblue") +
+  labs(title = "Modelo de regresión lineal",
+       x = "variable independiente (x)",
+       y = "variable dependiente (y)") +
+  theme(
+    panel.background = element_rect(fill = "black"),
+    panel.grid.major = element_line(color = "grey40"),
+    panel.grid.minor = element_line(color = "grey20"),
+    plot.background = element_rect(fill = "black"),
+    plot.title = element_text(color = "white"),
+    axis.title = element_text(color = "white"),
+    axis.text = element_text(color = "white")
+  )
+
+# -----------------------------------------------------------------------------
---
+++ 2026/MLMLG_2026/codigos/R/UMSA_MLMLG_2026_003.R
@@ -0,0 +1,87 @@
+# ==========================================================
+# Maestría en Ciencia y Análisis de Datos
+# Universidad Mayor de San Andrés
+# ----------------------------------------------------------
+#   Modelos lineales y modelos lineales generalizados
+# ----------------------------------------------------------
+#        Rolando Gonzales Martinez, Julio 2024
+# ==========================================================
+
+# Simulaciones de un modelo lineal multivariado
+
+# Cargar librerías necesarias
+library(ggplot2)
+library(broom) # install.packages("broom")
+
+# Configurar semilla para reproducibilidad
+set.seed()
+
+# Generar datos simulados
+n <- 100  # Número de observaciones
+x1 <- rnorm(n, mean = 50, sd = 10)  # Variable predictora 1
+x2 <- rnorm(n, mean = 30, sd = 5)   # Variable predictora 2
+
+# Parámetros:
+beta0 = 5
+beta1 = 2
+beta2 = 3
+error = (n, mean = 0, sd = 1) # Distribucion Gaussiana (Normal) estandar: rnorm
+
+# y = b0 + b1*x1 + b2*x2 + e
+---- ecuacion aqui ---------
+
+# Graficar histogramas de x1, x2 e y
+hist(x1)
+hist(x2)
+hist(y)
+
+# Combinar x1, x2 e y en un data frame
+datos <- data.frame(x1 = x1, x2 = x2, y = y)
+
+# Ajustar el modelo de regresión lineal
+modelo_multivariante <- lm(y ~ x1 + x2, data = datos)
+
+# Resumir el modelo
+summary(modelo_multivariante)
+
+# Obtener estimaciones intervalicas de los parámetros del modelo
+conf_intervals <- confint(modelo_multivariante)
+
+# Mostrar las estimaciones intervalicas
+print(conf_intervals)
+
+# Extraer datos para las bandas de predicción
+pred_data <- augment(modelo_multivariante, datos)
+
+# Graficar los datos y la línea de regresión con fondo negro, línea azul clara, y puntos amarillos
+ggplot(datos, aes(x = x1, y = y)) +
+  geom_point(color = "yellow") +
+  geom_smooth(method = "lm", aes(color = "x1"), se = TRUE) +
+  labs(title = "Modelo de regresión lineal con dos variables predictoras",
+       x = "Variable independiente x1",
+       y = "Variable dependiente (y)") +
+  theme(
+    panel.background = element_rect(fill = "black"),
+    panel.grid.major = element_line(color = "grey40"),
+    panel.grid.minor = element_line(color = "grey20"),
+    plot.background = element_rect(fill = "black"),
+    plot.title = element_text(color = "white"),
+    axis.title = element_text(color = "white"),
+    axis.text = element_text(color = "white")
+  )
+
+ggplot(datos, aes(x = x2, y = y)) +
+  geom_point(color = "yellow") +
+  geom_smooth(method = "lm", aes(color = "x2"), se = TRUE) +
+  labs(title = "Modelo de regresión lineal con dos variables predictoras",
+       x = "Variable independiente x2",
+       y = "Variable dependiente (y)") +
+  theme(
+    panel.background = element_rect(fill = "black"),
+    panel.grid.major = element_line(color = "grey40"),
+    panel.grid.minor = element_line(color = "grey20"),
+    plot.background = element_rect(fill = "black"),
+    plot.title = element_text(color = "white"),
+    axis.title = element_text(color = "white"),
+    axis.text = element_text(color = "white")
+  )
